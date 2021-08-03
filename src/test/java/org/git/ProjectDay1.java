72835b9686b3ec17e3dae1981e30a5500154f031
unknown <dhanupoorna@gmail.com>
2021-08-03 13:30:25 +0530

    poornnaaaa

--- src/test/java/org/git/ProjectDay1.java
+++ src/test/java/org/git/ProjectDay1.java
@@ -1,6 +1,11 @@
 package org.git;
 
 public class ProjectDay1 {
-	
-
+	public static  void venkat() {
+		// TODO Auto-generated method stub
+ System.out.println("venkat is soberi");
+	}
+public static void main(String[] args) {
+	venkat();
+}
 }
